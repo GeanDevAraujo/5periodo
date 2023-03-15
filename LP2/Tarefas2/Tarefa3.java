13e8babaa5f99827ad3e12892fe260d76b1d70a3
GeanDevAraujo <geandersondearaujopassos@gmail.com>
2023-03-15 09:12:55 -0300

    new update

--- LP2/Tarefas2/Tarefa3.java
+++ LP2/Tarefas2/Tarefa3.java
@@ -10,8 +10,10 @@ public class Tarefa3 {
         System.out.println("Fórmula da progressão Aritmética : an = a1 + (n – 1) . r");
         System.out.println("Digite o primeiro número da progressão:");
         a1 = scan.nextInt();
-        System.out.println("");
-
+        System.out.println(" Posição do termo na sequência que se deseja:");
+        n = scan.nextInt();
+        System.out.println("Qaul a razão(neste caso soma) entre os termos envolvidos.");
+        r = scan.nextInt();
         formula = a1 + (n - 1) * r;
         System.out.println("O enézimo número se encontra na posição " + formula);
     }
